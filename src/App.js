2531ba51a35e448236bf02bd9c3f956c97603445
Tosin Oyelami <tosin_oyelami@yahoo.com>
2022-11-16 10:03:22 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="" element={<Home />} />
+          <Route path="/Metabnb" element={<Home />} />
           <Route path="/place" element={<Place />} />
         </Routes>
       </BrowserRouter>
